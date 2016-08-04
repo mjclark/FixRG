b5e707c1abb2e590241d40c1d1219da005935c9a
mjc <michael.james.clark@gmail.com>
2016-08-03 22:02:17 -0700

    fixes to design

--- FixRG.pl
+++ FixRG.pl
@@ -5,18 +5,20 @@ use warnings;
 
 #USAGE
 if(@ARGV != 2) {
-	die("Usage: perl FixRG.pl <RAM [e.g. \"4G\"> <input.bam> <output_prefix>\n");
+	die("Usage: perl FixRG.pl <RAM [e.g. \"4G\"> <old.bam> <realigned.bam> <output_prefix>\n");
 }
 
 #INPUTS
-my $RAM = shift
-my $INBAM = shift;
+my $RAM = shift;
+my $OLDBAM = shift;
+my $REBAM = shift;
 my $PREFIX = shift;
 
 #PIPELINE
-#Step 1: Open input BAM file and read in header with samtools
+#Step 1: Open old BAM file and read in header with samtools
 
-#Step 2: Create temp files for each Readgroup and pump RG reads into each file
+
+#Step 2: Create temp files for each Readgroup and pump RG reads from realigned BAM into each file
 
 #Step 3: Use Picard to add RGs to each file based on RGs in original BAM's header
 
